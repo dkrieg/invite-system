4a511d19c2f298543d8404a78066521f56aecec5
Daniel Krieg <daniel_krieg@mac.com>
2022-09-10 10:54:55 -0500

    dockerize modules and fixed erroneous app name

--- management-ui/src/app/app.component.spec.ts
+++ management-ui/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sample-angular-app'`, () => {
+  it(`should have as title 'management-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sample-angular-app');
+    expect(app.title).toEqual('management-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('sample-angular-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('management-ui app is running!');
   });
 });
--- reservation-ui/src/app/app.component.spec.ts
+++ reservation-ui/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sample-angular-app'`, () => {
+  it(`should have as title 'reservation-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sample-angular-app');
+    expect(app.title).toEqual('reservation-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('sample-angular-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('reservation-ui app is running!');
   });
 });
